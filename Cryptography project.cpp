30e799e751cd8b760eb4534201b1362d6dbf2b48
OSAMA-MAHMOUD2000 <osamaom175@gmail.com>
2023-06-11 10:11:16 +0530

    Cryptography project

---
+++ Cryptography project.cpp
@@ -0,0 +1,117 @@
+// Cryptography project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+class party
+{
+	int n;
+	int g;
+	int lgNum;
+
+public:
+	party()
+	{
+		n = g = lgNum = 0;
+	}
+	void getdata();
+	int isPrime(int num);
+	int publicKey();
+	int privateKey(int a);
+	int display();
+};
+void party::getdata()
+{
+	int x, y, z;
+	cout << "Enter two prime numbers N,G and large number, please." << endl;
+
+	while (true)
+	{
+		cin >> x;
+		if (isPrime(x) == x)
+		{
+			n = x;
+			break;
+		}
+		else
+		{
+			cout << "your input(" << x << ") is not prime number, re-enter a prime number" << endl;
+			continue;
+		}
+	}
+	while (true)
+	{
+		cin >> y;
+		if (isPrime(y) == y)
+		{
+			g = y;
+			break;
+		}
+		else
+		{
+			cout << "your input(" << y << ") is not prime number, re-enter a prime number" << endl;
+			continue;
+		}
+	}
+	cin >> z;
+	lgNum = z;
+}
+int party::publicKey()
+{
+	int x = (int)pow(g, lgNum);
+	return x % n;
+}
+int party::privateKey(int a)
+{
+	int x = (int)pow(a, lgNum);
+	return x % n;
+}
+int party::isPrime(int num)
+{
+	int count = 0;
+	for (int i = 1; i < num; i++)
+	{
+		if (num % i == 0)
+		{
+			count = count + 1;
+		}
+	}
+	if (count == 1)
+	{
+		cout << num << " is prime number" << endl;
+		return num;
+	}
+	else
+		cout << "It's not prime number" << endl;
+	return -1;
+}
+
+int main()
+{
+	party ob1;
+	ob1.getdata();
+	int A = ob1.publicKey();
+	party ob2;
+	ob2.getdata();
+	int B = ob2.publicKey();
+
+	int k1 = ob1.privateKey(B);
+	int k2 = ob2.privateKey(A);
+	if (k1 == k2)
+	{
+		cout << "the keys exchange process has done successfully" << endl;
+	}
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started:
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
